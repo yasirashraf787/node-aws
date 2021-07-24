db60ab356957a8c76cbd053264ee903f927b8059
Yasir Khan <58009725+yasirashraf787@users.noreply.github.com>
2021-07-24 12:52:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req,res)=>{
+
+    res.send("Welcome to the home page");
+});
+
+app.listen(3000, () => {
+
+    console.log("Hello world");
+});
+
+

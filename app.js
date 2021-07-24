38f54b58c6586dda4b6d559314117ffd9f53d483
Yasir Khan <58009725+yasirashraf787@users.noreply.github.com>
2021-07-24 18:25:14 -0500

    updated the port

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.get('/', (req,res)=>{
     res.send("Welcome to the home page");
 });
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
 
     console.log("Hello world");
 });

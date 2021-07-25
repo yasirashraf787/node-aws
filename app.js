c557aaffb984a88ffd247308f03417e22725927e
Yasir Khan <58009725+yasirashraf787@users.noreply.github.com>
2021-07-25 11:57:47 -0500

    added database and query

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const { TopologyDescription } = require('mongodb');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    /*host: 'localhost',
+    user:'root',
+    password: 'root',
+    database: 'test'
+*/
+    host: 'letbknown-db.cbenxjbir7bt.us-east-2.rds.amazonaws.com',
+    user:'admin',
+    password: 'letbknown',
+    database: 'test'
+});
+
+db.connect((err)=>{
+  if(err){
+      throw err;
+  }
+  console.log('mysql connected');
+});
+
 const app = express();
 
-
+app.get('/fetchdata',(req, res) =>{
+ let sql = 'SELECT * FROM TEST WHERE TYPE = 1';
+ let query = db.query(sql, (err,results) => {
+     if(err) throw err;
+    let response = '';
+    for (var i = 0; i < results.length; i++) {
+        console.log(results[i]);
+        response = "data : " + "Type :" + results[i].type + ", Desc :"+results[i].desc
+      }
+    res.send(response);
+ })
+});
 app.get('/', (req,res)=>{
 
     res.send("Welcome to the home page 2.0 ####");
@@ -9,7 +42,6 @@ app.get('/', (req,res)=>{
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
-
     console.log("Hello world");
 });
 
